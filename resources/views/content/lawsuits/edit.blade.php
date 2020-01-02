24883feaf43fb38697bb925161c1a832ddf2b386
Le Dinh Thai <thaiattt@gmail.com>
2020-01-02 16:21:10 +0700

    [fix] ES linter edit lawsuits

--- resources/views/content/lawsuits/edit.blade.php
+++ resources/views/content/lawsuits/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', '事件を編集する画面')
 @section('content')
-    <lawsuits-edit :type_lawsuits="{{ $typeLawsuits }}"
-                   :lawsuit_id="{{ $lawsuitId }}"
+    <lawsuits-edit :typeLawsuits="{{ $typeLawsuits }}"
+                   :lawsuitId="{{ $lawsuitId }}"
     ></lawsuits-edit>
 @endsection
