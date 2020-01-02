62464f95a5c6050d9f9c3dc5e550236c9c1b39e3
du cong <cong.du.connectiv@gmail.com>
2020-01-02 14:21:09 +0700

    [fix] #6: delete a lawsuit

--- resources/js/components/lawsuits/index.vue
+++ resources/js/components/lawsuits/index.vue
@@ -6,8 +6,9 @@
         <h3 class="description"/>
       </v-col>
     </v-row>
-
+    
     <div class="overflow overflow-x-auto">
+      <div v-if="isShowDelete" class="overlay"></div>
       <table class="table">
         <thead>
         <tr class="title d-flex">
@@ -26,7 +27,33 @@
         </tr>
         </thead>
         <tbody>
-          <rangRowLawsuit v-for="(lawsuit, index) in lawsuits" :lawsuit="lawsuit" :index="index" :key="index" />
+        <template v-for="(lawsuit, index) in lawsuits">
+          <tr class="d-flex" @click="clickTR(lawsuit.id)" :key="lawsuit.id">
+            <td scope="col" class="col col-3">{{ lawsuit.number }}</td>
+            <td scope="col" class="col col-3">{{ lawsuit.name }}</td>
+            <td scope="col" class="col col-3">{{ lawsuit.courts_departments }}</td>
+            <td scope="col" class="col col-3">{{ convertString(lawsuit.defendants) }}</td>
+            <td scope="col" class="col col-3">{{ convertString(lawsuit.defendant_representatives) }}</td>
+            <td scope="col" class="col col-3">{{ convertString(lawsuit.plaintiffs) }}</td>
+            <td scope="col" class="col col-3">{{ convertString(lawsuit.plaintiff_representatives) }}</td>
+            <td scope="col" class="col col-3 d-flex pa-0 last-child-table" :class="{'unset-relative': isShowDelete}">
+              <div class="col-6"><div class="pl-3">-</div></div>
+              <v-spacer></v-spacer>
+              <div class="col-6 text-right col-btn font-weight-600 text-size-20">
+                <v-btn :id="'lawsuit-sub-menu-' + lawsuit.id" icon @click="clickBTN(index)" v-on:click.stop="" v-click-outside="hidden">...</v-btn>
+              </div>
+              <v-list class="sub-menu" :class="{ 'actived': activeIndex === index}">
+                <v-list-item>
+                  <v-list-item-title>名前を変更</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="deleteLawsuit(lawsuit.id)" v-on:click.stop="">
+                  <v-list-item-title>ファイルを削除</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </td>
+          </tr>
+          <app-delete-item :dataType="'lawsuits'" v-on:cancelSubmit="isShowDelete = $event"  v-if="isShowDelete" :data="dataReceived" />
+        </template>
         </tbody>
       </table>
     </div>
@@ -34,37 +61,104 @@
 </template>
 
 <script>
-  import rangRowLawsuit from './range-row-lawsuit'
-
+  import ClickOutside from 'vue-click-outside';
   export default {
     name: "Index",
-    components: {
-      rangRowLawsuit
+    directives: {
+      /**
+       * ClickOutside: Clicks Outside an Element
+       */
+      ClickOutside
     },
+    // props: {
+    //   lawsuits: { type: Array, required: false, default: () => [] }
+    // },
     data() {
       return {
-        lawsuits: {},
+        activeIndex: undefined,
+        i: 1,
+        isShowDelete: false,
+        dataReceived: null,
+        lawsuits: {}
       }
     },
     created() {
-      /**
-       * @function
-       * @description fetch list lawsuits data from API
-       */
-      axios.get('lawsuits')
-        .then(res => {
-          this.lawsuits = res.data.data;
-          console.log(this.lawsuits)
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      axios.get('lawsuits').then(res => {
+        return this.lawsuits = res.data.data;
+      })
     },
+    computed: {
+      testData() {
+      
+      }
+    },
+    methods: {
+      /**
+       * @function clickTR
+       * @description To click to a link
+       */
+      clickTR(lawsuit_id) {
+        return window.location.href = 'lawsuits/' + lawsuit_id;
+      },
+      
+      /**
+       * @function clickBTN
+       * @description To click to a button
+       */
+      clickBTN(index) {
+        this.i++;
+        if(this.i % 2 === 0){
+          this.activeIndex = index;
+        } else {
+          this.activeIndex = undefined;
+        }
+      },
+      
+      /**
+       * @function checkRoute
+       * @description Check route and active class when click on url
+       * @return boolean
+       */
+      checkRoute($url) {
+        return window.location.pathname === $url;
+      },
+      
+      /**
+       * @function convertString
+       * @description convert array to string
+       * @return string
+       */
+      convertString(arrays){
+        if(arrays.length > 0){
+          let values = arrays.map(value => { return value.name });
+          return values.join(', ');
+        } else {
+          return '-'
+        }
+        
+      },
+      deleteLawsuit(lawsuit_id){
+        this.isShowDelete = true;
+        this.i = 1;
+        this.activeIndex = undefined;
+        this.dataReceived = lawsuit_id;
+      },
+      
+      /**
+       * @function hidden
+       * @description To hidden a block
+       */
+      hidden() {
+        this.i = 1;
+        this.activeIndex = undefined;
+        this.isShowDelete = false;
+      },
+    }
   }
 </script>
 <style lang="scss">
-	.unset-relative{
-		position: unset !important;
-		pointer-events: none;
-	}
+  .unset-relative{
+    position: unset !important;
+    pointer-events: none;
+  }
 </style>
