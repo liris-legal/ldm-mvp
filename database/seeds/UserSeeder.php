12cba77786d5d6dd60a1d3a3a8b2be97a0142039
du cong <cong.du.connectiv@gmail.com>
2020-01-10 11:32:30 +0700

    [add] account user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
         DB::table('users')->truncate();
         $rows = [
             ['8968e76a-4945-40e8-a77a-29261f7cd2f3', 'thai.le.connectiv@gmail.com', $time ],
+            ['271d6beb-be76-48d7-bb93-24db5c408c8c', 'cong.du.connectiv+_1@gmail.com', $time ],
         ];
         foreach ($rows as $row) {
             DB::table('users')->insert([
