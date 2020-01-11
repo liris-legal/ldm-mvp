7d98da391bee3bd9ea86cfe4063d6ffcdf8d8f95
Le Dinh Thai <thaiattt@gmail.com>
2020-01-11 13:43:09 +0700

    [fix] sessions migrate

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,29 +34,12 @@
                             </div>
                         </div>
 
-{{--                        <div class="form-group row">--}}
-{{--                            <div class="col-md-6 offset-md-4">--}}
-{{--                                <div class="form-check">--}}
-{{--                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-{{--                                    <label class="form-check-label" for="remember">--}}
-{{--                                        {{ __('Remember Me') }}--}}
-{{--                                    </label>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
                         <div class="form-group row mb-0 btn-add-form-group">
                             <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary d-block">
                                     {{ __('ログイン') }}
                                 </button>
 
-{{--                                @if (Route::has('password.request'))--}}
-{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
-{{--                                        {{ __('Forgot Your Password?') }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
                             </div>
                         </div>
                     </form>
