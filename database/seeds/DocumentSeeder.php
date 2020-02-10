4113f11ae0dc654d0d8c8d8e79e9d2208b901c5b
Le Dinh Thai <thaiattt@gmail.com>
2020-02-10 13:43:45 +0700

    [fix] document seeder

--- database/seeds/DocumentSeeder.php
+++ database/seeds/DocumentSeeder.php
@@ -17,14 +17,14 @@ class DocumentSeeder extends Seeder
 
         DB::table('documents')->truncate();
         $rows = [
-            [ '00001', 1, 1, 1, null, '訴状', 3, 'App\Models\Plaintiff', '2020011803014931-訴状サンプル.pdf', $time],
-            [ '00001', 3, 2, 1, null, '証拠説明書', 1, 'App\Models\Defendant', '2020011803013631-証拠説明書サンプル.pdf', $time],
-            [ '00001', 6, 3, 1, null, '期日経過報告書', null, null, '2020011803015131-期日経過報告書サンプル.pdf', $time],
-            [ '00001', 1, 1, 1, null, '答弁書', 3, 'App\Models\Plaintiff', '2020011803015531-答弁書サンプル.pdf', $time],
+            [ '00001', 1, 1, 1, 1, '訴状', 3, 'App\Models\Plaintiff', '2020011803014931-訴状サンプル.pdf', $time],
+            [ '00001', 3, 2, 1, 1, '証拠説明書', 1, 'App\Models\Defendant', '2020011803013631-証拠説明書サンプル.pdf', $time],
+            [ '00001', 6, 3, 1, 1, '期日経過報告書', null, null, '2020011803015131-期日経過報告書サンプル.pdf', $time],
+            [ '00001', 1, 1, 1, 1, '答弁書', 3, 'App\Models\Plaintiff', '2020011803015531-答弁書サンプル.pdf', $time],
             [ '00001', 1, 2, 1, 1, '甲号証', 1, 'App\Models\Plaintiff', '2020011803012231-証拠説明書サンプル.pdf', $time],
             [ '00001', 1, 2, 1, 2, '甲号証', 1, 'App\Models\Plaintiff', '2020013007011731-証拠説明書サンプル.pdf', $time],
             [ '00001', 1, 2, 2, 1, '甲号証', 2, 'App\Models\Plaintiff', '2020013006013931-証拠説明書サンプル.pdf', $time],
-            [ '00001', 1, 2, 1, null, '証拠説明書', 1, 'App\Models\Plaintiff', '2020011803013931-証拠説明書サンプル.pdf', $time],
+            [ '00001', 1, 2, 1, 1, '証拠説明書', 1, 'App\Models\Plaintiff', '2020011803013931-証拠説明書サンプル.pdf', $time],
         ];
         foreach ($rows as $row) {
             DB::table('documents')->insert([
