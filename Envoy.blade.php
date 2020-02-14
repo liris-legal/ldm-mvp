b9d3115350efc09e0db54661af9574691e00ec8d
Le Dinh Thai <thaiattt@gmail.com>
2020-02-14 10:30:06 +0700

    [fix] disable migrate db

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -25,8 +25,10 @@
 
 @task('composer')
     echo "Build APP through docker"
+    # docker exec {{ $container }} bash -c "cd {{ $work_dir }} && composer self-update && \
+    #    composer install --no-progress --no-interaction && php artisan migrate:refresh --seed --force"
     docker exec {{ $container }} bash -c "cd {{ $work_dir }} && composer self-update && \
-        composer install --no-progress --no-interaction && php artisan migrate:refresh --seed --force"
+        composer install --no-progress --no-interaction"
 @endtask
 
 @task('yarn')
